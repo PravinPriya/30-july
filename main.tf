1c35ac4603fdd03a696ce27bb33b949b44cc8913
unknown <yogipravin565@gmail.com>
2025-06-30 23:54:44 +0530

    first added

---
+++ main.tf
@@ -0,0 +1,45 @@
+resource "azurerm_resource_group" "rg-block" {
+  name     = "shiva-rgs"
+  location = "central india"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "shivaaks"
+  location            = azurerm_resource_group.rg-block.location
+  resource_group_name = azurerm_resource_group.rg-block.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "shivas"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
+}
+  
+
+
+
+
+
+
+
+
